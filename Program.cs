d98ce7d8efaa81e1e74ce624a9a743df9cc70f7c
Viktorlazi <viktorlazi010@gmail.com>
2020-11-23 10:27:09 +0100

    hello world commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Internship_2_C_Sharp_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
